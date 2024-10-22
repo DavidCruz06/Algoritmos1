817c0159c215c89514bf75424e82439c58deca2a
DavidCruz06 <davidcruzulloa20@gmail.com>
2024-10-22 15:54:22 -0500

    agregue la variable result(100) para poder hacer la operacion con las variables num1 y num2 que son los datos que ingresa el usuario, y agregue el document.write para que se muestre el resultado de la operacion

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 let num1 = parseInt(prompt("Valor de la prenda"))
 let num2 = parseInt(prompt("Descuento"))
+
+var result = 100
+let mult = num1*num2/result
+
+document.write("El valor de la prenda queda en:", mult, "<br>")
